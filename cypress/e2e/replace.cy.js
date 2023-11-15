977680684ebdb8dd374cb6dc5e629742ca3d9aeb
Eder Coronado <edercoronado@Eders-MacBook-Pro.local>
2023-11-15 16:11:57 -0600

    Adding spec to test replace functionality - Creating removeFile command to remove file at the end of every test - Validating API response when replacing file

---
+++ cypress/e2e/replace.cy.js
@@ -0,0 +1,34 @@
+import Login from "../pageObjects/login";
+import HomePage from "../pageObjects/homepage";
+import Assets from "../pageObjects/assets";
+
+describe.only("Assets", () => {
+  const login = new Login();
+  const homePage = new HomePage();
+  const assets = new Assets();
+  let assetId;
+
+  beforeEach(() => {
+    cy.intercept("POST", "**/assets").as("upload");
+    cy.visit("/");
+    login.signIn("coreddin@gmail.com", "@Hangar18001");
+    homePage.getMySpace().click();
+    homePage.getAssets().click();
+    cy.uploadFile("frodo.jpeg");
+    assets.getUploadBtn().click();
+    cy.wait("@upload").then(({ response }) => {
+      assetId = response.body.id;
+    });
+  });
+
+  after(() => {
+    cy.removeFile(assetId)
+  })
+  it("Replace file", () => {
+    cy.intercept("POST", "**/assets").as("replaceFile");
+    assets.getFileContainer().click();
+    cy.get('[aria-label="Replace asset"]').click();
+    cy.replaceFile("saruman.jpeg");
+    cy.wait("@replaceFile").its("response.statusCode").should("eq", 200);
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,3 +3,20 @@ import 'cypress-file-upload'
 Cypress.Commands.add('uploadFile', (fileName) => {
     cy.get('#file').attachFile(fileName)
 })
+
+Cypress.Commands.add('replaceFile', (replaceFile) => {
+    cy.get('#replacefile').attachFile(replaceFile)
+})
+
+Cypress.Commands.add('removeFile', (fileId) => {
+    cy.request({
+        method: 'POST',
+        url: 'https://app.storyblok.com/v1/spaces/262308/assets/bulk_destroy',
+        headers: {
+            Authorization: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyMjAyMjEsInRpbWVzdGFtcCI6MTcwMDY5MDU5NX0.rwfLM7vovhdG5htw8tvDpulzS-weeLTwzecxkKo1dAU'
+        },
+        body: {
+            "ids":[fileId]
+        }
+    })
+})
